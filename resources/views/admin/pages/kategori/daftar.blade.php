483165a9d2960d7845cbeca58bb286d38a5219d7
maymayrpla <maymaymaymay73472@gmail.com>
2019-05-29 17:23:50 +0700

    episode 18-27

--- resources/views/admin/pages/kategori/daftar.blade.php
+++ resources/views/admin/pages/kategori/daftar.blade.php
@@ -1,82 +1,60 @@
 @extends('admin.main')
-@section('title','User')
+@section('title','Kategori')
 @section('content')
-<h1>User</h1>
+<h1>Kategori</h1>
 <hr>
 
-@if(session('result') == 'success')
-<div class="alert alert-success alert-dismissible fade show">
-	<strong>Saved!</strong> Berhasil Disimpan.
-	<button type="button" class="close" data-dismiss="alert">
-		&times;
-	</button>
+@if( session('result') == 'update')
+<div class="alert alert-success alert-dismiss fade show">
+	<strong>Updated!!</strong> Berhasil Diupdate.
+	<button type="button" class="close" data-dismiss="alert">&times</button>
 </div>
 @endif
 
-@if(session('result') == 'update')
-<div class="alert alert-success alert-dismissible fade show">
-	<strong>Updated!</strong> Berhasil Diupdate.
-	<button type="button" class="close" data-dismiss="alert">
-		&times;
-	</button>
-</div>
-@endif
-
-@if(session('result') == 'delete')
-<div class="alert alert-success alert-dismissible fade show">
+@if( session('result') == 'delete')
+<div class="alert alert-success alert-dismiss fade show">
 	<strong>Deleted!</strong> Berhasil Dihapus.
-	<button type="button" class="close" data-dismiss="alert">
-		&times;
-	</button>
+	<button type="button" class="close" data-dismiss="alert">&times</button>
 </div>
 @endif
 
-@if(session('result') == 'fail-delete')
-<div class="alert alert-danger alert-dismissible fade show">
-	<strong>Failed!</strong> Gagal Dihapus.
-	<button type="button" class="close" data-dismiss="alert">
-		&times;
-	</button>
+@if( session('result') == 'success')
+<div class="alert alert-success alert-dismiss fade show">
+	<strong>Saved!</strong> Berhasil Disimpan.
+	<button type="button" class="close" data-dismiss="alert">&times</button>
 </div>
 @endif
-
 
 <div class="row">
 	<div class="col-md-6 mb-3">
-		<a href="{{ route('admin.user.add') }}" class="btn btn-primary">[+] Tambah</a>
+		<a href="{{ route('admin.kategori.add') }}" class="btn btn-primary">[+] Tambah</a>
 	</div>
-
 	<div class="col-md-6 mb-3">
-		<form method="GET" action="{{ route('admin.user') }}">
+		<form method="GET" action="{{ route('admin.kategori') }}">
 			<div class="input-group">
-				<input type="text" name="keyword"
-				value="{{ request('keyword') }}"
-				class="form-control">
+				<input type="text" name="keyword" class="form-control" value="{{ request('keyword') }}">
 				<div class="input-group-append">
 					<button type="submit" class="btn btn-primary">
-						Cari !
+						Cari!
 					</button>
 				</div>
-			</div>
+			</div><!-- End Input Group -->
 		</form>
 	</div>
-</div>
+	
+</div><!-- End Row -->
 
 <table class="table table-striped mb-3">
 	<tr>
-		<th>Name</th><th>Email</th><th>Akses</th><th>&nbsp;</th>
+		<th>Kategori</th><th>&nbsp;</th>
 	</tr>
 	@foreach($data as $dt)
 	<tr>
-		<td>{{ $dt->name }}</td>
-		<td>{{ $dt->email }}</td>
-		<td>{{ $dt->akses }}</td>
+		<td>{{ $dt->nama_kategori }}</td>
 		<td>
-			<a href="{{ route('admin.user.edit',['id'=>$dt->id]) }}"
-			class="btn btn-success btn-sm">
+			<a href="{{route('admin.kategori.edit',['id'=>$dt->id])}}" class="btn btn-success btn-sm">
 				<i class="fa fa-w fa-edit"></i>
 			</a>
-
 			@if( $dt->id != Auth::id() )
 			<button class="btn btn-danger btn-sm btn-trash" 
 			data-id="{{ $dt->id }}"
@@ -84,7 +62,6 @@
 				<i class="fa fa-w fa-trash"></i>
 			</button>
 			@endif
-
 		</td>
 	</tr>
 	@endforeach
@@ -94,7 +71,6 @@
 	$data->appends( request()->only('keyword') )
 	->links('vendor.pagination.bootstrap-4')
 }}
-
 @endsection
 
 @push('modal')
@@ -110,7 +86,7 @@
 			</div><!-- End Modal Header -->
 			<div class="modal-body">
 				Apakah anda yakin ingin menghapusnya?
-				<form id="form-delete" method="post" action="{{ route('admin.user') }}">
+				<form id="form-delete" method="post" action="{{ route('admin.kategori') }}">
 					{{ csrf_field() }}
 					{{ method_field('delete') }}
 					<input type="hidden" name="id" id="input-id">
--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['middleware'=>['auth']], function(){
-	Route::prefix('admin')->group(function(){
+Route::group(['prefix'=>'admin','middleware'=>['auth']], function(){
 	Route::get('/', function(){
 		return view('admin.pages.dashboard');
 	})->name('admin.home');
 
+	/**/
 	Route::prefix('user')->group(function(){
 		Route::get('/','UserController@daftar')->name('admin.user')->middleware('akses.admin');
+		Route::delete('/','UserController@delete')->middleware('akses.admin');
 
 		Route::get('/add','UserController@add')->name('admin.user.add')->middleware('akses.admin');
 		Route::post('/add','UserController@save')->middleware('akses.admin');
@@ -35,9 +36,20 @@ Route::group(['middleware'=>['auth']], function(){
 		Route::get('/setting','UserSettingController@form')->name('admin.user.setting');
 		Route::post('/setting','UserSettingController@update');
 		});
-	});
+		/* Kategori */
+
+		Route::group(['prefix'=>'kategori','middleware'=>'akses.admin'], function(){
+				Route::get('/','KategoriController@daftar')->name('admin.kategori');
+				Route::get('/add','KategoriController@add')->name('admin.kategori.add');
+				Route::post('/add','KategoriController@save');
+				Route::get('/edit/{id}','KategoriController@edit')->name('admin.kategori.edit');
+				Route::post('/edit/{id}','KategoriController@update');
+				Route::get('/','KategoriController@daftar')->name('admin.kategori')->middleware('akses.admin');
+		Route::delete('/','KategoriController@delete')->middleware('akses.admin');
+		});
 
 });
+
 Auth::routes();
 
 Route::any('register', function(){ return abort(404); });
